e2c1b940d2d0b8d5f5f14d56da883deccb2c6fef
proalex <proallex@icloud.com>
2014-09-23 19:54:33 +0300

    Switch to double buffering output. All tabs replaced with spaces.

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -1,23 +1,20 @@
 #include <Windows.h>
 #include <Windowsx.h>
 #include <strsafe.h>
-#include <vector>
 #include "resource.h"
-
-using namespace std;
 
 struct Point
 {
-	int x;
-	int y;
+    int x;
+    int y;
 
-	Point(int _x, int _y) : x(_x), y(_y) {}
+    Point(int _x, int _y) : x(_x), y(_y) {}
 };
 
 enum Tool
 {
-	PEN,
-	LINE
+    PEN,
+    LINE
 };
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -33,31 +30,31 @@ Output:    VOID
 */
 VOID ErrorExit(LPTSTR lpszFunction)
 {
-	LPVOID lpMsgBuf;
-	LPVOID lpDisplayBuf;
-	DWORD dw = GetLastError();
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError();
 
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER |
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dw,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf,
-		0, NULL);
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
 
-	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
-		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
-	StringCchPrintf((LPTSTR)lpDisplayBuf,
-		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
-		TEXT("%s failed with error %d: %s"),
-		lpszFunction, dw, lpMsgBuf);
-	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+    StringCchPrintf((LPTSTR)lpDisplayBuf,
+        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+        TEXT("%s failed with error %d: %s"),
+        lpszFunction, dw, lpMsgBuf);
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
 
-	LocalFree(lpMsgBuf);
-	LocalFree(lpDisplayBuf);
-	ExitProcess(dw);
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+    ExitProcess(dw);
 }
 
 
@@ -67,26 +64,26 @@ Function:  RegisterWindowClass
 Purpose:   Register window class with default parameters
 
 Input:     LPSTR lpszClassName - name of the window class
-WNDPROC lpfnWndProc - pointer to window callback function for message handling
+           WNDPROC lpfnWndProc - pointer to window callback function for message handling
 
 Output:    BOOL - TRUE on success, FALSE on failure
 */
 BOOL RegisterWindowClass(LPSTR lpszClassName, WNDPROC lpfnWndProc)
 {
-	WNDCLASSEX wc;
+    WNDCLASSEX wc;
 
-	memset(&wc, 0, sizeof(wc));
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
-	wc.hInstance = GetModuleHandle(NULL);
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wc.lpszClassName = lpszClassName;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
-	return RegisterClassEx(&wc);
+    memset(&wc, 0, sizeof(wc));
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wc.lpszClassName = lpszClassName;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+    return RegisterClassEx(&wc);
 }
 
 /*
@@ -95,191 +92,166 @@ Function:  CreateDefaultWindow
 Purpose:   Create window with default parameters
 
 Input:     LPSTR lpszClassName - name of the window class
-LPSTR lpszWindowName - window name
-INT nCmdShow - specifies how the window is to be shown
+           LPSTR lpszWindowName - window name
+           INT nCmdShow - specifies how the window is to be shown
 
 Output:    BOOL - TRUE on success, FALSE on failure
 */
 BOOL CreateDefaultWindow(LPSTR lpszClassName, LPSTR lpszWindowName, INT nCmdShow)
 {
-	HWND hMainWnd;
+    HWND hMainWnd;
 
-	hMainWnd = CreateWindow(lpszClassName, lpszClassName, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, GetModuleHandle(NULL), 0);
+    hMainWnd = CreateWindow(lpszClassName, lpszClassName, WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, GetModuleHandle(NULL), 0);
 
-	if (hMainWnd)
-	{
-		ShowWindow(hMainWnd, nCmdShow);
-		UpdateWindow(hMainWnd);
-	}
+    if (hMainWnd)
+    {
+        ShowWindow(hMainWnd, nCmdShow);
+        UpdateWindow(hMainWnd);
+    }
 
-	return hMainWnd ? 1 : 0;
+    return hMainWnd ? 1 : 0;
 }
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
 {
-	MSG msg;
-	LPSTR lpszClassName = "lab1";
+    MSG msg;
+    LPSTR lpszClassName = "lab1";
 
-	if (!RegisterWindowClass(lpszClassName, WndProc))
-		ErrorExit("RegisterClassEx");
+    if (!RegisterWindowClass(lpszClassName, WndProc))
+        ErrorExit("RegisterClassEx");
 
-	if (!CreateDefaultWindow(lpszClassName, lpszClassName, nCmdShow))
-		ErrorExit("CreateWindow");
+    if (!CreateDefaultWindow(lpszClassName, lpszClassName, nCmdShow))
+        ErrorExit("CreateWindow");
 
-	while (GetMessage(&msg, 0, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+    while (GetMessage(&msg, 0, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-	return msg.wParam;
+    return msg.wParam;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	static HDC hDC;
-	static int x, y, xLine, yLine, xLine2, yLine2;
-	static vector<Point> curve;
-	static vector<vector<Point>> curves;
-	static BOOL bTracking;
-	static HMENU hMenu;
-	static Tool currentTool = PEN;
-	vector<Point>::iterator it;
+    static HDC hDC, bufferedDC, tempDC;
+    static HBITMAP bufferedBMP, tempBMP;
+    static int beginX, beginY, endX, endY, width, height;
+    static BOOL bTracking = FALSE;
+    static HMENU hMenu;
+    static Tool currentTool = PEN;
+    static RECT rect;
 
-	switch (uMsg)
-	{
-	case WM_CREATE:
-		hMenu = GetMenu(hWnd);
-		hDC = GetDC(hWnd);
-		break;
-	case WM_LBUTTONDOWN:
-		bTracking = TRUE;
-		x = GET_X_LPARAM(lParam);
-		y = GET_Y_LPARAM(lParam);
-		MoveToEx(hDC, x, y, NULL);
+    switch (uMsg)
+    {
+    case WM_CREATE:
+        hMenu = GetMenu(hWnd);
+        hDC = GetDC(hWnd);
+        GetClientRect(hWnd, &rect);
+        bufferedDC = CreateCompatibleDC(hDC);
+        tempDC = CreateCompatibleDC(hDC);
+        width = rect.right - rect.left;
+        height = rect.bottom - rect.top;
+        bufferedBMP = CreateCompatibleBitmap(hDC, width, height);
+        SelectObject(bufferedDC, bufferedBMP);
+        tempBMP = CreateCompatibleBitmap(hDC, width, height);
+        SelectObject(tempDC, tempBMP);
+        PatBlt(bufferedDC, 0, 0, width, height, WHITENESS);
+        break;
+    case WM_LBUTTONDOWN:
+        bTracking = TRUE;
+        beginX = GET_X_LPARAM(lParam);
+        beginY = GET_Y_LPARAM(lParam);
+        BitBlt(tempDC, 0, 0, width, height, bufferedDC, 0, 0, SRCCOPY);
+        MoveToEx(tempDC, beginX, beginY, NULL);
+        break;
+    case WM_LBUTTONUP:
+        if (bTracking)
+        {
+            bTracking = FALSE;
+            BitBlt(bufferedDC, 0, 0, width, height, tempDC, 0, 0, SRCCOPY);
+        }
 
-		switch (currentTool)
-		{
-		case PEN:
-			curve.push_back(Point(x, y));
-			break;
-		case LINE:
-			SetROP2(hDC, R2_NOTXORPEN);
-			xLine = x;
-			yLine = y;
-			xLine2 = x;
-			yLine2 = y;
-			break;
-		}
+        break;
+    case WM_MOUSEMOVE:
+        if (bTracking)
+        {
+            endX = GET_X_LPARAM(lParam);
+            endY = GET_Y_LPARAM(lParam);
 
-		break;
-	case WM_LBUTTONUP:
-		if (bTracking)
-		{
-			bTracking = FALSE;
+            switch (currentTool)
+            {
+            case PEN:
+                LineTo(tempDC, endX, endY);
+                break;
+            case LINE:
+                BitBlt(tempDC, 0, 0, width, height, bufferedDC, 0, 0, SRCCOPY);
+                MoveToEx(tempDC, beginX, beginY, NULL);
+                LineTo(tempDC, endX, endY);
+                break;
+            }
 
-			switch (currentTool)
-			{
-			case PEN:
-				curves.push_back(curve);
-				curve.clear();
-			case LINE:
-				SetROP2(hDC, R2_COPYPEN);
-				curve.push_back(Point(xLine, yLine));
-				curve.push_back(Point(xLine2, yLine2));
-				curves.push_back(curve);
-				curve.clear();
-				break;
-			}
-		}
+            InvalidateRect(hWnd, &rect, FALSE);
+            UpdateWindow(hWnd);
+        }
 
-		break;
-	case WM_MOUSEMOVE:
-		if (bTracking)
-		{
-			x = GET_X_LPARAM(lParam);
-			y = GET_Y_LPARAM(lParam);
+        break;
+    case WM_PAINT:
+        HDC hdc;
+        PAINTSTRUCT ps;
 
-			switch (currentTool)
-			{
-			case PEN:
-				LineTo(hDC, x, y);
-				curve.push_back(Point(x, y));
-				break;
-			case LINE:
-				MoveToEx(hDC, xLine, yLine, NULL);
-				LineTo(hDC, xLine2, yLine2);
-				xLine2 = x;
-				yLine2 = y;
-				MoveToEx(hDC, xLine, yLine, NULL);
-				LineTo(hDC, xLine2, yLine2);
-				break;
-			}
-		}
+        hdc = BeginPaint(hWnd, &ps);
 
-		break;
-	case WM_PAINT:
-		HDC hdc;
-		PAINTSTRUCT ps;
+        if (bTracking)
+            BitBlt(hdc, 0, 0, width, height, tempDC, 0, 0, SRCCOPY);
+        else
+            BitBlt(hdc, 0, 0, width, height, bufferedDC, 0, 0, SRCCOPY);
 
-		hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case ID_FILE_EXIT:
+            SendMessage(hWnd, WM_DESTROY, 0, 0);
+            break;
+        case ID_TOOL_PEN:
+            CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_PEN, MF_CHECKED);
+            CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_LINE, MF_UNCHECKED);
+            currentTool = PEN;
+            break;
+        case ID_TOOL_LINE:
+            CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_PEN, MF_UNCHECKED);
+            CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_LINE, MF_CHECKED);
+            currentTool = LINE;
+            break;
+        case ID_CLEAR:
+            if (!bTracking)
+            {
+                HBRUSH clearBrush = CreateSolidBrush(RGB(0, 0, 0));
 
-		for (size_t i = 0; i < curves.size(); i++)
-		{
-			it = curves[i].begin();
-			MoveToEx(hdc, it->x, it->y, NULL);
+                PatBlt(bufferedDC, 0, 0, width, height, WHITENESS);
+                BitBlt(hDC, 0, 0, width, height, bufferedDC, 0, 0, SRCCOPY);
+                InvalidateRect(hWnd, &rect, TRUE);
+                UpdateWindow(hWnd);
+            }
 
-			for (it + 1; it != curves[i].end(); it++)
-				LineTo(hdc, it->x, it->y);
-		}
+            break;
+        }
+    
+        break;
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
+    case WM_DESTROY:
+        ReleaseDC(hWnd, hDC);
+        DeleteDC(bufferedDC);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
 
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case ID_FILE_EXIT:
-			SendMessage(hWnd, WM_DESTROY, 0, 0);
-			break;
-		case ID_TOOL_PEN:
-			CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_PEN, MF_CHECKED);
-			CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_LINE, MF_UNCHECKED);
-			currentTool = PEN;
-			break;
-		case ID_TOOL_LINE:
-			CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_PEN, MF_UNCHECKED);
-			CheckMenuItem(GetSubMenu(hMenu, 1), ID_TOOL_LINE, MF_CHECKED);
-			currentTool = LINE;
-			break;
-		case ID_CLEAR:
-			if (!bTracking)
-			{
-				RECT rect;
-
-				for (size_t i = 0; i < curves.size(); i++)
-					curves[i].clear();
-
-				curves.clear();
-				GetClientRect(hWnd, &rect);
-				InvalidateRect(hWnd, &rect, TRUE);
-				UpdateWindow(hWnd);
-			}
-
-			break;
-		}
-	
-		break;
-	case WM_CLOSE:
-		DestroyWindow(hWnd);
-		break;
-	case WM_DESTROY:
-		ReleaseDC(hWnd, hDC);
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, uMsg, wParam, lParam);
-	}
-
-	return 0;
+    return 0;
 }
