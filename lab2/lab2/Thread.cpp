d494f59690e11c45307af293b6aa5a2d5492f29a
ProAlex <proallex@icloud.com>
2014-10-25 18:59:09 +0300

    Fixed typo.

--- lab2/lab2/Thread.cpp
+++ lab2/lab2/Thread.cpp
@@ -17,15 +17,16 @@ DWORD WINAPI ThreadEntryPoint(LPVOID lpParam)
 			Task task = threadPool->GetNextTask();
 			LeaveCriticalSection(&threadPool->taskQueue);
 			InterlockedDecrement(&threadPool->nFreeThreads);
+			task.threadIndex = threadIndex;
 			task.taskState = STATE_RUNNING;
 			string logMessage = "Thread with index " + threadIndex;
 			logMessage += " have got new task.";
 			threadPool->WriteLog(logMessage);
 			Job *job = (Job*)(task.GetFunction());
-			DWORD result = job(task.GetArgs());
+			task.returnValue = job(task.GetArgs());
 			InterlockedIncrement(&threadPool->nFreeThreads);
 			logMessage = "Thread with index " + threadIndex;
-			logMessage += " have done the task. Return value: " + result;
+			logMessage += " have done the task. Return value: " + task.returnValue;
 			threadPool->WriteLog(logMessage);
 			task.taskState = STATE_COMPLETED;
 		}
